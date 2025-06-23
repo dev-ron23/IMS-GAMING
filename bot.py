acfd654bc25272ad16580027efe3837e71f08cb8
dev-ron23 <btwitzron23@gmail.com>
2025-06-23 20:09:23 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,18 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")  # ✅ Correct variable name
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is online!")
+
+bot.run(DISCORD_TOKEN)  # ✅ Make sure the token is correct
